b95f900c8201b6467b086eab57b167f6bf7fbbb1
Mwaniki <mr.mwaniki@gmail.com>
2019-05-31 15:13:31 +0300

    prototype Engineer add and Site add

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import models.Post;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -9,61 +12,80 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
-        get("/", (request, response) -> {
-           Map<String, Object> model = new HashMap<String, Object>();//new model is made to store information
-            return new ModelAndView(model, "hello.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        get("/favorite_photos",(request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            return new ModelAndView(new HashMap(), "favorite_photos.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        get("/form", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            return new ModelAndView(model, "form.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        get("/greeting_card", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            String recipient = request.queryParams("recipient");
-            String sender = request.queryParams("sender");
-            model.put("recipient", recipient);
-            model.put("sender", sender);
-            return new ModelAndView(model, "greeting_card.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        get("/form2", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            return new ModelAndView(model, "form2.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        get("/engineer.hbs", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            String recipient = request.queryParams("engineer");
-            String sender = request.queryParams("sites");
-            String ek = request.queryParams("ek");
-            model.put("engineer", recipient);
-            model.put("ek", ek);
-            model.put("sites", sender);
-            return new ModelAndView(model, "engineer.hbs");
-        }, new HandlebarsTemplateEngine());
 
         get("/",(request, response) -> {
-            Map<String, Object> model = new HashMap<String,Object>();
-            model.put("username", request.session().attribute("username"));
-            return new ModelAndView(model, "welcome.hbs");
+            Map<String,Object> model = new HashMap<>();
+            ArrayList<Post> posts = Post.getAll();
+            model.put("posts", posts);
+
+            return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
-        post("/welcome",(request, response) -> {
-            Map<String,Object> model = new HashMap<String, Object>();
+        post("/posts/new",(request, response) -> {
+            Map<String,Object> model = new HashMap<String,Object>();
+            String content = request.queryParams("content");
+            Post newPost = new Post(content);
+            return new ModelAndView(model,"success.hbs");
+        },  new HandlebarsTemplateEngine());
 
-            String inputtedUsername = request.queryParams("username");
-            request.session().attribute("username", inputtedUsername);
-            model.put("username", inputtedUsername);
 
-            return new ModelAndView(model, "welcome.hbs");
-        }, new HandlebarsTemplateEngine());
+
+//first form
+//        get("/", (request, response) -> {
+//           Map<String, Object> model = new HashMap<String, Object>();//new model is made to store information
+//            return new ModelAndView(model, "hello.hbs");
+//        }, new HandlebarsTemplateEngine());
+//
+//        get("/favorite_photos",(request, response) -> {
+//            Map<String, Object> model = new HashMap<String, Object>();
+//            return new ModelAndView(new HashMap(), "favorite_photos.hbs");
+//        }, new HandlebarsTemplateEngine());
+//
+//        get("/form", (request, response) -> {
+//            Map<String, Object> model = new HashMap<String, Object>();
+//            return new ModelAndView(model, "form.hbs");
+//        }, new HandlebarsTemplateEngine());
+//
+//        get("/greeting_card", (request, response) -> {
+//            Map<String, Object> model = new HashMap<String, Object>();
+//            String recipient = request.queryParams("recipient");
+//            String sender = request.queryParams("sender");
+//            model.put("recipient", recipient);
+//            model.put("sender", sender);
+//            return new ModelAndView(model, "greeting_card.hbs");
+//        }, new HandlebarsTemplateEngine());
+//
+//        get("/form2", (request, response) -> {
+//            Map<String, Object> model = new HashMap<String, Object>();
+//            return new ModelAndView(model, "form2.hbs");
+//        }, new HandlebarsTemplateEngine());
+//
+//        get("/engineer.hbs", (request, response) -> {
+//            Map<String, Object> model = new HashMap<String, Object>();
+//            String recipient = request.queryParams("engineer");
+//            String sender = request.queryParams("sites");
+//            String ek = request.queryParams("ek");
+//            model.put("engineer", recipient);
+//            model.put("ek", ek);
+//            model.put("sites", sender);
+//            return new ModelAndView(model, "engineer.hbs");
+//        }, new HandlebarsTemplateEngine());
+//
+//        get("/",(request, response) -> {
+//            Map<String, Object> model = new HashMap<String,Object>();
+//            model.put("username", request.session().attribute("username"));
+//            return new ModelAndView(model, "welcome.hbs");
+//        }, new HandlebarsTemplateEngine());
+//
+//        post("/welcome",(request, response) -> {
+//            Map<String,Object> model = new HashMap<String, Object>();
+//
+//            String inputtedUsername = request.queryParams("username");
+//            request.session().attribute("username", inputtedUsername);
+//            model.put("username", inputtedUsername);
+//
+//            return new ModelAndView(model, "welcome.hbs");
+//        }, new HandlebarsTemplateEngine());
 
 
 
--- src/main/java/models/Post.java
+++ src/main/java/models/Post.java
@@ -1,8 +1,26 @@
 package models;
 
-public class Post {
-    public Post(String content) {
+import java.util.ArrayList;
 
+public class Post {
+    private String content;
+    private static ArrayList<Post> instances = new ArrayList<>();
+
+    public Post(String content) {
+        this.content = content;
+        instances.add(this);
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public static ArrayList<Post> getAll() {
+        return instances;
+    }
+
+    public static void clearAllPosts() {
+        instances.clear();
     }
 }
 
